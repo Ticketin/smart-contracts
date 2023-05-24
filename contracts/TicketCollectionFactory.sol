776c5311648a5850464763fbae1f30feb58d7d79
Bram <Bram.mathijssen@gmail.com>
2023-05-24 16:49:06 +0200

    feat: added feature for admin to keep track of all created events

--- contracts/TicketCollectionFactory.sol
+++ contracts/TicketCollectionFactory.sol
@@ -15,8 +15,8 @@ contract TicketCollectionFactory {
     // Collection counter to keep track of all created events i.c.w idToCollection mapping
     Counters.Counter public collectionCounter;
 
-    // TODO: Probably need to make this a nested mapping OR a addres => address[]
-    mapping(address => address) public adminToCollection;
+    // Mapping to keep track of all events created by a admin
+    mapping(address => address[]) public adminToCollection;
 
     // Mapping to keep track of all created contracts by every admin
     mapping(uint256 => address) public idToCollection;
@@ -37,22 +37,23 @@ contract TicketCollectionFactory {
         );
         ticketCollection.transferOwnership(msg.sender);
 
-        adminToCollection[msg.sender] = address(ticketCollection);
-        // adminToCollection[msg.sender].push(address(ticketCollection));
+        adminToCollection[msg.sender].push(address(ticketCollection));
         idToCollection[collectionCounter.current()] = address(ticketCollection);
         collectionCounter.increment();
 
         return address(ticketCollection);
     }
 
-    // https://coinsbench.com/how-to-get-array-data-in-solidity-without-using-for-loop-6c023fbd5896
-    // function getDeployedCollections() public view returns (address[] memory) {
-    //     address[] memory ticketCollectionAddresses = new address[](
-    //         adminToCollection[msg.sender].length
-    //     );
-    //     for (uint i = 0; i < adminToCollection[msg.sender].length; i++) {
-    //         ticketCollectionAddresses[i] = adminToCollection[msg.sender][i];
-    //     }
-    //     return ticketCollectionAddresses;
-    // }
+    // gets values from the adminToCollection() mapping by address
+    function getDeployedCollections(
+        address _adminAddress
+    ) public view returns (address[] memory) {
+        address[] memory ticketCollectionAddresses = new address[](
+            adminToCollection[_adminAddress].length
+        );
+        for (uint i = 0; i < adminToCollection[_adminAddress].length; i++) {
+            ticketCollectionAddresses[i] = adminToCollection[_adminAddress][i];
+        }
+        return ticketCollectionAddresses;
+    }
 }
