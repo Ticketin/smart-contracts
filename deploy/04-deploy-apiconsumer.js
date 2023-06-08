0549627941e9ff32081a7cc0dd05d71905dd5bf6
therne <retail3210@gmail.com>
2023-06-08 23:23:21 +0900

    feat: Deploy PockyAPIConsumer

---
+++ deploy/04-deploy-apiconsumer.js
@@ -0,0 +1,21 @@
+const { network } = require('hardhat');
+
+module.exports = async ({ getNamedAccounts, deployments, ethers }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const pockyCollections = await ethers.getContract('PockyCollections');
+
+  log('----------------------------------------------------');
+  const deployResult = await deploy('PockyAPIConsumer', {
+    from: deployer,
+    args: [pockyCollections.address],
+    waitConfirmations: 1,
+  });
+  if (deployResult.newlyDeployed) {
+    log(`contract PockyAPIConsumer deployed at ${deployResult.address} using ${deployResult.receipt.gasUsed} gas`);
+  }
+  log(`granting PockyAPIConsumer.RESULT_ORACLE_ROLE to PockyAPIConsumer (${deployResult.address})`);
+  await pockyCollections.grantRole(await pockyCollections.RESULT_ORACLE_ROLE(), deployResult.address);
+};
+
+module.exports.tags = ['PockyAPIConsumer'];
